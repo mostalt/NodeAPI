e74011427f0c7f7249c51dd2781d6215f66420f7
mostalt <mostalt.mail@gmail.com>
2016-12-01 18:45:49 +0300

    remove any files

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
+var path = require('path');;
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -8,24 +7,16 @@ var log = require('./libs/log')(module);
 
 var index = require('./routes/index');
 var articles = require('./routes/articles');
-var users = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.use(index);
 app.use(articles);
 
 // catch 404 and forward to error handler
